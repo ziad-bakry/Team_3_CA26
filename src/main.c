60ab63ae47effb2fe8844b3fa362803e20aa823b
yaragehad <yaragehad06@gmail.com>
2026-05-29 18:21:33 +0200

    Fixed merge erros

--- src/main.c
+++ src/main.c
@@ -29,7 +29,6 @@
 
 /* el struct de betmassel el instruction w kol el fields beta3etha
    ba3d ma te3melho decode mn el raw binary */
-
 typedef struct {
     int valid;
     int32_t raw_instruction;
@@ -72,43 +71,6 @@ int clock_cycle;
 int total_instructions;
 
 
-DecodedInstruction decode_raw(int32_t raw, int pc_of_instr) {
-    DecodedInstruction d;
-    d.valid             = 1;
-    d.result            = 0;
-    d.mem_address       = 0;
-    d.ID_cycles_done = 0;
-    d.EX_cycles_done = 0;
-    d.pc_of_instruction = pc_of_instr;
-
-
-    d.opcode = (raw >> 28) & 0xF;
-    d.r1 = (raw >> 23) & 0x1F;  /* masking done in all but req in anyth other than opcode since andy values at their left */
-    d.r2 = (raw >> 18) & 0x1F;
-    d.r3 = (raw >> 13) & 0x1F;
-    d.shamt = raw & 0x1FFF; /* hena msh benetar n shift ashan aslan el shamt fl a5er fa no reason to shift*/
-
-    int32_t raw_imm = raw & 0x3FFFF;
-    if (raw_imm & (1 << 17))
-        d.immediate = raw_imm | 0xFFFC0000;
-    else
-        d.immediate = raw_imm;
-
-
-    d.address = raw & 0x0FFFFFFF;
-
-    // chooses the correct formate
-    if (d.opcode == OP_ADD || d.opcode == OP_SUB || d.opcode == OP_MUL ||
-        d.opcode == OP_AND || d.opcode == OP_LSL || d.opcode == OP_LSR)
-        d.format = FORMAT_R;
-    else if (d.opcode == OP_JMP)
-        d.format = FORMAT_J;
-    else
-        d.format = FORMAT_I;
-
-    return d;
-}
-
 
 int32_t encode_instruction(char* line) {
     char mnemonic[10];  /* ashan mayeb2oosh 3ala ad bzabty w ye7sal buffer overflow */
@@ -150,6 +112,7 @@ int32_t encode_instruction(char* line) {
         sscanf(line, "%s R%d R%d %d", mnemonic, &r1, &r2, &shamt);
         encoded = (OP_LSR << 28) | (r1 << 23) | (r2 << 18) | (shamt & 0x1FFF);
     }
+
     /* I-FORMAT INSTRUCTIONS */
 
     else if (strcmp(mnemonic, "MOVI") == 0) {
@@ -200,6 +163,7 @@ int32_t encode_instruction(char* line) {
     return encoded;
 }
 
+
 void load_program(const char* filename) { //filename depends on the name of the actual instruction file
     FILE* f = fopen(filename, "r");
     if (!f) {
@@ -228,14 +192,54 @@ void load_program(const char* filename) { //filename depends on the name of the 
     printf("Loaded %d instructions.\n\n", total_instructions);
 }
 
+
+DecodedInstruction decode_raw(int32_t raw, int pc_of_instr) {
+    DecodedInstruction d;
+    d.valid             = 1;
+    d.result            = 0;
+    d.mem_address       = 0;
+    d.ID_cycles_done = 0;
+    d.EX_cycles_done = 0;
+    d.pc_of_instruction = pc_of_instr;
+
+
+    d.opcode = (raw >> 28) & 0xF;
+    d.r1 = (raw >> 23) & 0x1F;  /* masking done in all but req in anyth other than opcode since andy values at their left */
+    d.r2 = (raw >> 18) & 0x1F;
+    d.r3 = (raw >> 13) & 0x1F;
+    d.shamt = raw & 0x1FFF; /* hena msh benetar n shift ashan aslan el shamt fl a5er fa no reason to shift*/
+
+    int32_t raw_imm = raw & 0x3FFFF;
+    if (raw_imm & (1 << 17))
+        d.immediate = raw_imm | 0xFFFC0000;
+    else
+        d.immediate = raw_imm;
+
+
+    d.address = raw & 0x0FFFFFFF;
+
+    // chooses the correct formate
+    if (d.opcode == OP_ADD || d.opcode == OP_SUB || d.opcode == OP_MUL ||
+        d.opcode == OP_AND || d.opcode == OP_LSL || d.opcode == OP_LSR)
+        d.format = FORMAT_R;
+    else if (d.opcode == OP_JMP)
+        d.format = FORMAT_J;
+    else
+        d.format = FORMAT_I;
+
+    return d;
+}
+
+
 void stage_fetch() {
     if (PC > INSTRUCTION_MEM_END) {  /*1024 limit*/
         IF_reg.valid = 0;
         return;
     }
 
+
     if (memory[PC] == 0 && PC >= total_instructions) { /*eg 5 instruction so mesh bast3ml whole memory*/
-     IF_reg.valid = 0;
+        IF_reg.valid = 0;
         return;
     }
 
@@ -250,6 +254,7 @@ void stage_fetch() {
     printf("  [IF] PC=%d | Raw=0x%08X | Fetched\n",
            fetched_pc, (uint32_t)raw); /*u yaani unsigned*/
 }
+
 
 void stage_decode() {
     if (!ID_reg.valid) {
@@ -283,123 +288,6 @@ void stage_decode() {
                ID_reg.r1, ID_reg.r2, ID_reg.vr2, ID_reg.immediate);
     else
         printf(" | ADDR=%d\n", ID_reg.address);
-}
-
-int is_mem_active_this_cycle() {
-    return (MEM_reg.valid &&
-            (MEM_reg.opcode == OP_MOVR || MEM_reg.opcode == OP_MOVM));
-} /*3shan law mem active mesh hadkhol fetch*/
-
-
-int pipeline_done() {
-    if (clock_cycle == 1) return 0;
-
-    int fetch_done = (PC >= total_instructions);
-    int pipeline_empty = (!IF_reg.valid && !ID_reg.valid &&
-                          !EX_reg.valid && !MEM_reg.valid);
-    return fetch_done && pipeline_empty;
-}
-
-void run_pipeline() {
-
-    while (!pipeline_done()) {
-
-        printf("\n---------- Clock Cycle %d ----------\n", clock_cycle);
-
-
-        WB_reg = MEM_reg;
-        MEM_reg.valid = 0;
-
-        if (EX_reg.valid && EX_reg.EX_cycles_done == 2) {
-            MEM_reg = EX_reg;
-            EX_reg.valid = 0;
-        }
-
-        if (!EX_reg.valid && ID_reg.valid && ID_reg.ID_cycles_done == 2) {
-            EX_reg = ID_reg;
-            ID_reg.valid = 0;
-     }
-
-        /* ID gets IF only when ID is empty */
-        if (!ID_reg.valid && IF_reg.valid) {
-            ID_reg       = IF_reg;
-            IF_reg.valid = 0;
-        }
-
-
-        stage_writeback();
-        stage_memory();
-        stage_execute();
-        stage_decode();
-
-        if (clock_cycle % 2 == 1) { //odd
-            if (!is_mem_active_this_cycle())
-                stage_fetch();
-            else
-                printf("  [IF] Stalled (MEM using memory this cycle)\n");
-        } else {
-            if(!IF_reg.valid)
-                printf("  [IF] Empty\n");
-        }
-
-        clock_cycle++;
-
-    }
-}
-
-void stage_memory() {
-
-    if (!MEM_reg.valid) {
-        printf("  [MEM] Empty\n");
-        return;
-    }
-
-    DecodedInstruction d = MEM_reg;
-
-    if (d.opcode == OP_MOVR) {
-        int actual_addr = d.mem_address + DATA_MEM_START;
-        MEM_reg.result  = memory[actual_addr];
-        printf("  [MEM] MOVR: Load MEM[%d] = %d into R%d\n",
-               actual_addr, MEM_reg.result, d.r1);
-    }
-    else if (d.opcode == OP_MOVM) {
-        int actual_addr     = d.mem_address + DATA_MEM_START;
-        memory[actual_addr] = d.result; /*result calculated in execute movm*/
-        printf("  [MEM] MOVM: Store %d into MEM[%d]\n",
-               d.result, actual_addr);
-        printf("  >> MEM[%d] = %d\n", actual_addr, d.result);
-    }
-    else {
-
-        printf("  [MEM] No memory operation (opcode=%d)\n", d.opcode);
-    }
-}
-
-void stage_writeback() {
-
-    if (!WB_reg.valid) {
-        printf("  [WB] Empty\n");
-        return;
-    }
-
-    DecodedInstruction d = WB_reg;
-
-    int writes_register = (d.opcode == OP_ADD  || d.opcode == OP_SUB  ||
-                           d.opcode == OP_MUL  || d.opcode == OP_MOVI ||
-                           d.opcode == OP_AND  || d.opcode == OP_XORI ||
-                           d.opcode == OP_LSL  || d.opcode == OP_LSR  ||
-                           d.opcode == OP_MOVR);
-
-    if (writes_register) {
-        if (d.r1 == 0) {
-            printf("  [WB] Destination is R0 (zero register) --> no write\n");
-        } else {
-            registers[d.r1] = d.result;
-            printf("  [WB] R%d = %d\n", d.r1, d.result);
-        }
-    } else {
-        printf("  [WB] No register write (opcode=%d)\n", d.opcode);
-    }
 }
 
 
@@ -514,6 +402,130 @@ void stage_execute()
         printf("  >> Branch taken: PC updated to %d | Pipeline flushed\n", PC);
     }
 }
+
+void stage_memory() {
+
+    if (!MEM_reg.valid) {
+        printf("  [MEM] Empty\n");
+        return;
+    }
+
+    DecodedInstruction d = MEM_reg;
+
+    if (d.opcode == OP_MOVR) {
+        int actual_addr = d.mem_address + DATA_MEM_START;
+        MEM_reg.result  = memory[actual_addr];
+        printf("  [MEM] MOVR: Load MEM[%d] = %d into R%d\n",
+               actual_addr, MEM_reg.result, d.r1);
+    }
+    else if (d.opcode == OP_MOVM) {
+        int actual_addr     = d.mem_address + DATA_MEM_START;
+        memory[actual_addr] = d.result; /*result calculated in execute movm*/
+        printf("  [MEM] MOVM: Store %d into MEM[%d]\n",
+               d.result, actual_addr);
+        printf("  >> MEM[%d] = %d\n", actual_addr, d.result);
+    }
+    else {
+
+        printf("  [MEM] No memory operation (opcode=%d)\n", d.opcode);
+    }
+}
+
+
+void stage_writeback() {
+
+    if (!WB_reg.valid) {
+        printf("  [WB] Empty\n");
+        return;
+    }
+
+    DecodedInstruction d = WB_reg;
+
+    int writes_register = (d.opcode == OP_ADD  || d.opcode == OP_SUB  ||
+                           d.opcode == OP_MUL  || d.opcode == OP_MOVI ||
+                           d.opcode == OP_AND  || d.opcode == OP_XORI ||
+                           d.opcode == OP_LSL  || d.opcode == OP_LSR  ||
+                           d.opcode == OP_MOVR);
+
+    if (writes_register) {
+        if (d.r1 == 0) {
+            printf("  [WB] Destination is R0 (zero register) --> no write\n");
+        } else {
+            registers[d.r1] = d.result;
+            printf("  [WB] R%d = %d\n", d.r1, d.result);
+        }
+    } else {
+        printf("  [WB] No register write (opcode=%d)\n", d.opcode);
+    }
+}
+
+
+
+int is_mem_active_this_cycle() {
+    return (MEM_reg.valid &&
+            (MEM_reg.opcode == OP_MOVR || MEM_reg.opcode == OP_MOVM));
+} /*3shan law mem active mesh hadkhol fetch*/
+
+
+int pipeline_done() {
+    if (clock_cycle == 1) return 0;
+
+    int fetch_done = (PC >= total_instructions);
+    int pipeline_empty = (!IF_reg.valid && !ID_reg.valid &&
+                          !EX_reg.valid && !MEM_reg.valid);
+    return fetch_done && pipeline_empty;
+}
+
+void run_pipeline() {
+
+    while (!pipeline_done()) {
+
+        printf("\n---------- Clock Cycle %d ----------\n", clock_cycle);
+
+
+        WB_reg = MEM_reg;
+        MEM_reg.valid = 0;
+
+        if (EX_reg.valid && EX_reg.EX_cycles_done == 2) {
+            MEM_reg = EX_reg;
+            EX_reg.valid = 0;
+        }
+
+        if (!EX_reg.valid && ID_reg.valid && ID_reg.ID_cycles_done == 2) {
+            EX_reg = ID_reg;
+            ID_reg.valid = 0;
+     }
+
+        /* ID gets IF only when ID is empty */
+        if (!ID_reg.valid && IF_reg.valid) {
+            ID_reg       = IF_reg;
+            IF_reg.valid = 0;
+        }
+
+
+        stage_writeback();
+        stage_memory();
+        stage_execute();
+        stage_decode();
+
+        if (clock_cycle % 2 == 1) { //odd
+            if (!is_mem_active_this_cycle())
+                stage_fetch();
+            else
+                printf("  [IF] Stalled (MEM using memory this cycle)\n");
+        } else {
+            if(!IF_reg.valid)
+                printf("  [IF] Empty\n");
+        }
+
+        clock_cycle++;
+
+    }
+}
+
+
+
+
 void print_final_state() {
     printf("\n");
     printf("--------FINAL PROCESSOR STATE--------"" \n");
@@ -549,4 +561,15 @@ void print_final_state() {
 }
 
 
+int main() {
 
+    clock_cycle= 1;
+
+    load_program("instructions.asm");
+
+    printf("Starting pipeline simulation...\n");
+    run_pipeline();
+
+    print_final_state();
+    return 0;
+}
